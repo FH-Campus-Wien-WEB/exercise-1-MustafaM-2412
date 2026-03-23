cbb832046f3380f09453cee2890f182a39a36fc6
MustafaM-2412 <Mustafa.Maksuti@gmail.com>
2026-03-23 12:00:19 +0100

    part 1,2,3 :D

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const app = express()
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
+const jsonPath = require('../ressources/movie.json');
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
   // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+  // res.send('!dlrow olleH')
+  // wir wollen hier auf unsere ressources/movie.json zugreifen und dann wenn die GET actionmehtode aufgerufen wird das der ausgegeben wird
+  res.json(jsonPath);
 })
 
 app.listen(3000)
